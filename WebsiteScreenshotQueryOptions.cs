9048975ee2f2b90dab24e71e180f16fad5de5130
APIVerve Team <hello@apiverve.com>
2024-08-20 13:34:28 -0500

    Bug Fixes

---
+++ WebsiteScreenshotQueryOptions.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APIVerve
+{
+    public class WebsiteScreenshotQueryOptions {
+public string url { get; set; }
+public string type { get; set; }
+public int width { get; set; }
+public int height { get; set; }
+public bool fullpage { get; set; }
+}
+
+}
